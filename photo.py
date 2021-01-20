0e6dda92adcf3ca6d299a07f4e96744242ee3012
Grace <xrzhang.li@yahoo.com>
2021-01-20 14:01:33 -0500

    Retrieve metadata from img

---
+++ photo.py
@@ -0,0 +1,22 @@
+#this only works on JPEG images
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+# path to the image or video
+imagename = "./IMG_5999.JPG"
+
+# read the image data using PIL
+image = Image.open(imagename)
+
+# extract EXIF data
+exifdata = image.getexif()
+
+# iterating over all EXIF data fields
+for tag_id in exifdata:
+    # get the tag name, instead of human unreadable tag id
+    tag = TAGS.get(tag_id, tag_id)
+    data = exifdata.get(tag_id)
+    # decode bytes 
+    if isinstance(data, bytes):
+        data = data.decode()
+    print(f"{tag:25}: {data}")
